b38fb4eadb84631d1611a49e0f924a407b7a66c1
kanata-git-hub <kanata840@gmail.com>
2026-07-06 03:48:48 +0900

    Update server.ts

--- server.ts
+++ server.ts
@@ -7,7 +7,7 @@ dotenv.config({ override: true });
 
 async function startServer() {
   const app = express();
-  const PORT = process.env.PORT || 3000;
+  const PORT = process.env.PORT ? parseInt(process.env.PORT, 10) : 3000;
 
   app.use(express.json({ limit: '50mb' }));
 
